c8b46f3ed3d3b80ffdaa5147ddd18a6c113110a3
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-28 00:19:50 +0600

    chore: Update FilterButton styling and count badge appearance

--- app/components/filter/FilterButton.js
+++ app/components/filter/FilterButton.js
@@ -3,9 +3,9 @@ import React from 'react';
 const FilterButton = ({ label, count, active }) => {
   return (
     <button
-      className={`flex items-center py-2.5 px-2 rounded-md border ${active ? 'bg-blue-100 text-blue-600 border-blue-600' : 'bg-white text-gray-600 border-gray-300'} hover:bg-gray-100 transition`}
+      className={`flex items-center py-2.5 px-2 rounded-md ${active ? 'bg-[#F3F8FF] text-[#2166F0] ' : 'bg-white text-gray-600 border border-gray-300'} hover:bg-gray-100 transition`}
     >
-      {label} <span className="ml-2 text-[#2166F0] bg-[#F3F8FF] text-[11px] font-bold px-1.5 py-1">{count}</span>
+      {label} <span className={`ml-2 rounded-[3px] ${active? "bg-[#2166F0] text-white":"text-[#2166F0] bg-[#F3F8FF]"}  text-[11px] font-bold px-1.5 py-1`}>{count}</span>
     </button>
   );
 };
