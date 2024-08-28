601060f45daf85f813e1eb95ee0c29dfd1f7a445
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-28 16:19:23 +0600

    refactor: Update OrderDatePicker component to filter orders based on selected date

--- app/components/filter/OrderDatePicker.js
+++ app/components/filter/OrderDatePicker.js
@@ -1,11 +1,29 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client"
-import React, { useState } from 'react';
+import { useOrderStore } from '@/store/ordersStore';
+import React, { useState,useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const OrderDatePicker = () => {
 
+    const { ordersList,tampOrdersList, setOrderList } = useOrderStore();
+
     const [startDate, setStartDate] = useState(null);
+
+    useEffect(() => {
+        if (startDate) {
+            const filtered = tampOrdersList.filter(order => {
+                const orderDate = new Date(order.createdAt.$date);
+                return orderDate.toDateString() === startDate.toDateString();
+            });
+
+            setOrderList(filtered);
+        } else {
+            
+            setOrderList(tampOrdersList);
+        }
+    }, [startDate]);
 
     const handleDateChange = (date) => {
         setStartDate(date);
@@ -23,3 +41,5 @@ const OrderDatePicker = () => {
 }
 
 export default OrderDatePicker
+
+
